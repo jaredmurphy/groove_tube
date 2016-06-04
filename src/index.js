1787bdb3c0f06579467f1a7ec173d0a52b257ee9
Jared <jaredcmurphy@gmail.com>
2016-06-04 12:29:14 -0400

    youtube api key obtained

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
 const API_KEY = "AIzaSyDxDSX_ofdJVSkdSm2XZuo6NNgUZluXQ6c";
 //import { Component } from 'react';
