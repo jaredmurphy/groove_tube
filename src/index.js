674cf06f3378ea0c016430d775dd004cb7fd8392
Jared <jaredcmurphy@gmail.com>
2016-06-04 13:45:48 -0400

    app component now a class rather than function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = "AIzaSyDxDSX_ofdJVSkdSm2XZuo6NNgUZluXQ6c";
 
-YTSearch({key: API_KEY, term: 'higher order functions'},
-  function(data){
-    console.log(data);
-  }
-)
+YTSearch({key: API_KEY, term: 'higher order functions'}, function(data){
+  console.log(data);
+});
 
-const App = () => {
-    return (
-      <div>
-        <h1>GrooveTube</h1>
-        <SearchBar />
-      </div>
-    );
+class App extends Component {
+    render() {
+      return (
+        <div>
+          <h1>GrooveTube</h1>
+          <SearchBar />
+        </div>
+      );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
