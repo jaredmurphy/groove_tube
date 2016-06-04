657e9fdcf8f0d4c40e07feb05ab921f0aac79709
Jared <jaredcmurphy@gmail.com>
2016-06-04 14:15:37 -0400

    video items, video list, and app components talking successfuly and rendering data on the dom

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+  const VideoItems = props.videos.map( (video) => {
+    return <VideoListItem video={video} />
+  });
+
   return (
-    <ul className="col-md-4-group">
-      {props.videos.length}
+    <ul className="col-md-4-group list-group">
+      {VideoItems}
     </ul>
   );
-}
+};
 
 export default VideoList;
