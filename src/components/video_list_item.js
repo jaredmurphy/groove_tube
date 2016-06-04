de9306ae2eb9dfe7b03b54c85e946e88059d9c39
Jared <jaredcmurphy@gmail.com>
2016-06-04 16:59:09 -0400

    adds more styling to app child components

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
-  const video = props.video;
-  const onVideoSelect = props.onVideoSelect;
+const VideoListItem = ({video, onVideoSelect}) => {
   const imageUrl = video.snippet.thumbnails.default.url;
   return (
-    <li onClick={() => onVideoSelect(video)} className="List-group-item">
+    <li onClick={() => onVideoSelect(video)} className="list-group-item">
       <div className="video-list media">
         <div className="media-left">
           <img className="media-object" src={imageUrl} />
