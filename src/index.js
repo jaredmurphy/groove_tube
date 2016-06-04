a5e91cdc85f2f97742d8b08453cb950690957c80
Jared <jaredcmurphy@gmail.com>
2016-06-04 11:58:44 -0400

    first react component displays on the dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Component } from 'react';
 
+
 // create a new component
+const App = function() {
+    return <div>GrooveTube</div>;
+}
+
+
 
 //take component's html and put in in the dom
+ReactDOM.render(<App />, document.querySelector('.container'));
