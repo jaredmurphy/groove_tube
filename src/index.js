73c74833fa982dfbf0b8ec11dabefb6cd0e87519
Jared <jaredcmurphy@gmail.com>
2016-06-04 18:06:39 -0400

    throttles search bar

--- src/index.js
+++ src/index.js
@@ -28,10 +28,11 @@ class App extends Component {
     }
 
     render() {
+      const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300 );
       return (
         <div>
           <h1>GrooveTube</h1>
-          <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+          <SearchBar onSearchTermChange={ videoSearch } />
           <VideoDetail video={this.state.selectedVideo} />
           <VideoList
             onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
